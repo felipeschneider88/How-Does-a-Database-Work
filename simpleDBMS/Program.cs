e0b31200610a2d69e0ea60603eef2f0d2b74e166
felipeschneider88 <felipes88@gmail.com>
2021-03-09 18:12:08 -0300

    first step

---
+++ simpleDBMS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace simpleDBMS
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string inputBuffer = null;
+            while (true)
+            {
+                Console.WriteLine("db > ");
+                inputBuffer = Console.ReadLine();
+                if (String.Compare(inputBuffer, ".exit") == 0)
+                {
+                    Environment.Exit(-1);
+                }
+                else
+                {
+                    Console.WriteLine("Unrecognized command {0}", inputBuffer);
+                }
+            }
+        }
+    }
+}
